80338822a91de3b8ebf5346ca772d260dde074d8
Warri <warriisaac@outlook.com>
2019-10-31 14:43:48 +0100

    props component usage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line
+const myfirstelement = <h1>Hello React!</h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const table = (
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>John</td>
+      </tr>
+      <tr>
+        <td>Elsa</td>
+      </tr>
+    </table>
+  );
+  class Car extends React.Component {
+      constructor() {
+          super();
+          this.state = {color: "red"};
+      }
+
+
+    render() {
+      return <h2>Hi, I am a {this.props.color} Car!</h2>;
+    }
+  }
+
+ReactDOM.render(
+        <Car color="red" />, 
+        document.getElementById('root')
+    );
