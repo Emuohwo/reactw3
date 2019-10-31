929cef87696f8f310868a53cae19100c0b92e7c7
Warri <warriisaac@outlook.com>
2019-10-31 17:06:17 +0100

    Using the state Object

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import ReactDOM from 'react-dom';
 class Computer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {brand: "Dell"};
+        this.state = {
+            brand: "Dell",
+            model: "Inspiron",
+            color: "black"
+        };
     }
 
     render() {
         return (
             <div>
-                <h1>Specfy the sate in the constructor method</h1>
-                <p>I am curruently using {this.state.brand} Computer</p>
+                <h1>Specify all the STATE properties your componenet need in the constructor method</h1>
+                <p>I am curruently using a {this.state.color} {this.state.brand} {this.state.model} Computer</p>
             </div>
         )
     }
