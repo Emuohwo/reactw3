954bb4b8a89dae7fcb1867fd32b8ebc85477d1b2
Warri <warriisaac@outlook.com>
2019-11-01 02:09:09 +0100

    componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Computer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favoritecolor: "black"
-        };
-    }
-    static getDerivedStateFromProps(props, state) {
-        return {favoritecolor: props.favcol }
-    }
+class Colors extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {favoritecolor: "silver"}
+  }
 
-    render() {
-        return (
-            <div>
-                {/* <h5>Specify all the STATE properties your componenet need in the constructor method</h5> */}
-                <h2>
-                    I am currently using a black Dell Computer 
-                    
-                    </h2>
-                    <h3>Although my favorite color is {this.state.favoritecolor} </h3>
-                
-            </div>
-        )
-    }
+  componentDidMount() {
+      setTimeout(() => {
+          this.setState({favoritecolor: "Gold"})
+      }, 2000)
+  }
+
+  render() {
+      return <h3>{this.state.favoritecolor} is my favourite color</h3>
+  }
 }
-
-ReactDOM.render(
-        <Computer favcol="skuyblue" />, 
-        document.getElementById('root')
-    );
+  
+ReactDOM.render(<Colors />, document.getElementById('root'));
