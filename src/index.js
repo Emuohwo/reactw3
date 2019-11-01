dde4b6796f23d58d4253c855ef598a72a32f37c1
Warri <warriisaac@outlook.com>
2019-11-01 08:02:25 +0100

    Updating lifecycle with getDerivedStateFromProps

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import ReactDOM from 'react-dom';
 class Colors extends React.Component {
   constructor(props) {
       super(props);
-      this.state = {favoritecolor: "silver"}
+      this.state = {favoritecolor: "silver"};
   }
 
-  componentDidMount() {
-      setTimeout(() => {
-          this.setState({favoritecolor: "Gold"})
-      }, 2000)
+  static getDerivedStateFromProps(props, state) {
+      return {favoritecolor: props.favcol}
   }
+ changeColor = () => {
+     this.setState({favoritecolor: "green"})
+ }
 
   render() {
-      return <h3>{this.state.favoritecolor} is my favourite color</h3>
+      return (
+          <div>
+            <h3>{this.state.favoritecolor} is my favourite color</h3>
+            <button type="button" onClick={this.changeColor}
+            >Click me </button>
+        </div>
+        )
   }
 }
   
-ReactDOM.render(<Colors />, document.getElementById('root'));
+ReactDOM.render(<Colors favcol="red" />, document.getElementById('root'));
+
+/*
+This example has a button that changes the favorite color to green,
+but since the getDerivedStateFromProps() method is called,
+the favorite color is still rendered as red
+(because the method updates the state
+with the color from the favcol attribute).
+*/
